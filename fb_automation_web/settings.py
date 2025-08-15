372ae99ac79eea30da9b67c8bd9e37a1305485fd
Ali Anwar <136829489+AliAnwar10@users.noreply.github.com>
2025-08-15 18:17:44 +0500

    Update settings.py

--- fb_automation_web/settings.py
+++ fb_automation_web/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jvm04^*ttbudv+p3h_po0#9=@@kdm4iha*+o3(=xyoalj2&4&q'
+SECRET_KEY = 'Your KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -105,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/Karachi'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
